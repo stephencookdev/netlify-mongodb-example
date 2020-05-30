a0b63272da314781e173e7e91398fb2d0a71d5fb
Stephen Cook <stephen@stephencookdev.co.uk>
2020-05-30 18:37:13 +0100

    Add POST support to pokemon

--- lambda_functions/pokemon.js
+++ lambda_functions/pokemon.js
@@ -27,11 +27,33 @@ const queryDatabase = async (db) => {
   };
 };
 
+const pushToDatabase = async (db, data) => {
+  const pokemonData = {
+    name: data.name,
+    number: data.number,
+  };
+
+  if (pokemonData.name && pokemonData.number) {
+    await db.collection("pokemon").insertMany([data]);
+    return { statusCode: 201 };
+  } else {
+    return { statusCode: 422 };
+  }
+};
+
 module.exports.handler = async (event, context) => {
   // otherwise the connection will never complete, since
   // we keep the DB connection alive
   context.callbackWaitsForEmptyEventLoop = false;
 
   const db = await connectToDatabase(MONGODB_URI);
-  return queryDatabase(db);
+
+  switch (event.httpMethod) {
+    case "GET":
+      return queryDatabase(db);
+    case "POST":
+      return pushToDatabase(db, JSON.parse(event.body));
+    default:
+      return { statusCode: 400 };
+  }
 };
