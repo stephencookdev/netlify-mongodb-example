0d99ae5b51b03af1b2b4f1134389ea8457661533
Stephen Cook <stephen@stephencookdev.co.uk>
2020-05-30 11:49:08 +0100

    Add a pokemon DB endpoint

---
+++ lambda_functions/pokemon.js
@@ -0,0 +1,37 @@
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db("test");
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db) => {
+  const pokemon = await db.collection("pokemon").find({}).toArray();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pokemon),
+  };
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+  return queryDatabase(db);
+};
